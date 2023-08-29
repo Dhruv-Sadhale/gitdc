3e22134577d7f0629bf64040f0b7789f473b9454
Dhruv <dhruv.sadhale@gmail.com>
2023-08-29 14:30:47 +0530

    added and modified all files of dsa_first_main successfully

---
+++ dsa_first.c
@@ -0,0 +1,6 @@
+#include "dsa_first.h"
+int add(int a , int b){
+int sum;
+sum=a+b;
+return sum;
+}
---
+++ dsa_first_main.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include "dsa_first.c"
+int main(){
+    printf("addition of two numbers are\n");
+    int x =add(2,3);
+    printf("%d",x);
+    return 0;
+}
