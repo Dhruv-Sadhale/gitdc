c4eb1690cc4e3f216e72b630ace1a8109c4774c2
Dhruv <dhruv.sadhale@gmail.com>
2023-08-31 15:40:01 +0530

    learnt about cat function and updated gitcommands

--- binary_search.c
+++ binary_search.c
@@ -22,14 +22,14 @@ if(low>high){
     return -1;
 }
 }
-void sort(int arr[],int size)
-{
-    for(int i=0;i<size;i++){
-        if(arr[i+1]>arr[i])
-        {
-            arr
-        }
+// void sort(int arr[],int size)
+// {
+//     for(int i=0;i<size;i++){
+//         if(arr[i+1]>arr[i])
+//         {
+//             arr
+//         }
 
-    }
-}
+//     }
+// }
 
